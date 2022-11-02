63a36948fae71dc8de8e9319f6bb93bf8b46fb0b
Anthony Naudts <anthonynaudts@hotmail.com>
2022-11-02 18:21:39 -0400

    Horario usuarios

--- registro.php
+++ registro.php
@@ -13,7 +13,7 @@
 
     <section class="section">
       <div class="row d-flex justify-content-center">
-        <div class="col-lg-6">
+        <div class="col">
 
           <div class="card">
             <div class="card-body">
@@ -22,14 +22,14 @@
 
               <form id="formularioRegistro" onsubmit="ActualizarUsuario(event)" class="row g-3 needs-validation" novalidate>
               
-                    <div class="col-12">
+                    <div class="col-6">
                       <label for="yourName" class="form-label">Nombre</label>
                       <input type="text" name="name" class="form-control" id="yourName" required>
                       <div class="invalid-feedback">¡Por favor, escriba su nombre!</div>
                     </div>
 
-                    <div class="col-12">
-                        <label class="col-sm-2 col-form-label">Posición</label>
+                    <div class="col-6">
+                        <label class="col-sm-2 col-form-label pt-0">Posición</label>
                             <select class="form-select" aria-label="Default select example" name="YourPosition" id="YourPosition" required>
                                 <option value="" selected>-Seleccionar posición</option>
                                 <!-- <option value="1">One</option> -->
@@ -37,39 +37,69 @@
                             </select>
                     </div>
 
-                    <div class="col-12">
+                    <div class="col-6">
                       <label for="yourEmail" class="form-label">Correo</label>
                       <input type="email" name="email" class="form-control" id="yourEmail" required>
                       <div class="invalid-feedback">¡Por favor, introduce una dirección de correo electrónico válida!</div>
                     </div>
 
-                    <div class="col-12">
+                    <div class="col-6">
                       <label for="yourEmail" class="form-label">Celular</label>
                       <input type="text" name="text" class="form-control" id="yourPhone" required>
                       <div class="invalid-feedback">¡Por favor, introduce el número de celular!</div>
                     </div>
 
-                    <div class="col-12 has-validation">
+                    <div class="col-6 has-validation">
                       <label for="yourUsername" class="form-label">Nombre de usuario</label>
                       <input type="text" name="username" class="form-control" id="yourUsername" required disabled>
                       <div class="invalid-feedback">Por favor, elija un nombre de usuario.</div>
                     </div>
 
-                    <div class="col-12">
+                    <div class="col-6">
                       <label for="yourPassword" class="form-label">Contraseña</label>
                       <input type="password" name="password" class="form-control" id="yourPassword" required>
                       <div class="invalid-feedback">¡Por favor, introduzca su contraseña!</div>
                     </div>
 
-                    <div class="col-12">
+                    <div class="col-6 has-validation d-flex flex-wrap justify-content-around">
+                      <div class="pe-2 w-50">
+                        <label for="yourUsername" class="form-label">Hora de entrada</label>
+                        <input type="time" name="username" class="form-control" id="horaEntrada" required>
+                        <div class="invalid-feedback">Por favor, elija un nombre de usuario.</div>
+                      </div>
+                      <div class="w-50">
+                        <label for="yourUsername" class="form-label">Hora de Salida</label>
+                        <input type="time" name="username" class="form-control" id="horaSalida" required>
+                        <div class="invalid-feedback">Por favor, elija un nombre de usuario.</div>
+                      </div>
+                    </div>
+
+                    <!-- <div class="col-6">
                       <div class="form-check">
                         <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms">
                         <label class="form-check-label" for="acceptTerms">Este usuario puede <a href="#">acceder</a> a la web.</label>
                         <div class="invalid-feedback">Debe estar de acuerdo antes de enviar.</div>
                       </div>
-                    </div>
+                    </div> -->
+
+                    
+                    
+
+                    <!-- <div class="col-6 has-validation">
+                      <label for="yourUsername" class="form-label">Hora de salida</label>
+                      <input type="time" name="username" class="form-control" id="yourUsername" required>
+                      <div class="invalid-feedback">Por favor, elija un nombre de usuario.</div>
+                    </div> -->
+
                     <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Registrar usuario</button>
+                      <div class="col-6">
+                        <div class="form-check">
+                          <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms">
+                          <label class="form-check-label" for="acceptTerms">Este usuario puede <a href="#">acceder</a> a la web.</label>
+                          <div class="invalid-feedback">Debe estar de acuerdo antes de enviar.</div>
+                        </div>
+                      </div>
+                      <button class="btn btn-primary col-6" type="submit">Registrar usuario</button>
                     </div>
                   </form>
 
